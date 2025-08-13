b506f604bb59bc042c67aff53848d85d3dc6cb6f
AlgoRythm-Dylan <dperunko@comcast.net>
2025-08-13 00:12:21 -0400

    Beginning of the science!

---
+++ EvolutionaryAlgorithms/MK1/Synth.cs
@@ -0,0 +1,9 @@
+﻿namespace MK1
+{
+    internal class Synth
+    {
+        public int Generation { get; set; }
+        public List<Synth> Ancestors { get; set; } = new();
+        public World? World { get; set; } = null;
+    }
+}
---
+++ EvolutionaryAlgorithms/MK1/World.cs
@@ -0,0 +1,13 @@
+﻿namespace MK1
+{
+    internal class World
+    {
+        public int Generation { get; set; } = 0;
+        public List<Synth> Population { get; set; } = new();
+
+        public void MaterializeInitialPopulation()
+        {
+
+        }
+    }
+}
